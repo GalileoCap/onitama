5d277823a51c2196a772c71d223ef1e007c47443
GalileoCap <galileocapp@gmail.com>
2023-02-28 20:45:11 -0300

    OK: Full move (and sides!)

--- src/gameSlice.jsx
+++ src/gameSlice.jsx
@@ -5,15 +5,38 @@ export const MINE = 0; export const THEIRS = 1;
 export const PAWN = 0; export const KING = 1;
 
 function isMine(from, board) {
-  return board[from.row][from.col] !== null; //TODO: Assing a side to each player
+  const state = board[from.row][from.col];
+  return state !== null && state.team === MINE;
 }
 
 function canMove(from, to, move, board) {
-  return true; //TODO: Check not same team and move allows it
+  const fromState = board[from.row][from.col];
+  const toState = board[to.row][to.col];
+
+  const dy = from.row - to.row;
+  const dx = from.col - to.col;
+
+  const notSameTeam = (fromState !== null && fromState.team === MINE) && (toState === null || toState.team === THEIRS);
+  let moveAllowsIt = false;
+  for (let { x, y } of move) { // Move allows it
+    moveAllowsIt |= x === dx && y === dy;
+  }
+
+  return notSameTeam && moveAllowsIt;
 }
 
 function transform({ row, col }) {
-  return { row, col }; //TODO: Transform
+  const f = (x) => { //TODO: Smarter?
+    switch (x) {
+    case 0: return 4;
+    case 1: return 3;
+    case 2: return 2;
+    case 3: return 1;
+    case 4: return 0;
+    }
+  }
+
+  return { row: f(row), col: f(col) };
 }
 
 function performMove(from, to, state) {
@@ -32,7 +55,6 @@ export const gameSlice = createSlice({
     move: undefined,
     board: [
       [{piece: PAWN, team: THEIRS}, {piece: PAWN, team: THEIRS}, {piece: KING, team: THEIRS}, {piece: PAWN, team: THEIRS}, {piece: PAWN, team: THEIRS}],
-      [null, null, null, null, null],
       [null, null, null, null, null],
       [null, null, null, null, null],
       [null, null, null, null, null],
@@ -58,7 +80,7 @@ export const gameSlice = createSlice({
 
       if (from === undefined && isMine(to, state.board)) { // Choose a piece
         state.cell = to;
-      } else if (state.move !== undefined && canMove(from, to, state.move, state.board)) { // Move
+      } else if (state.cell !== undefined && state.move !== undefined && canMove(from, to, state.move, state.board)) { // Move
         performMove(from, to, state);
         Conn.send({type: 'move', from, to});
       } else { // De-select
