ea55602924dfd6be33b524ed8a4e052d2d0987ce
GalileoCap <galileocapp@gmail.com>
2023-03-01 14:40:53 -0300

    OK: Board style

--- src/Game/Board.jsx
+++ src/Game/Board.jsx
@@ -1,5 +1,22 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { selectCell, KING } from './gameSlice';
+import { selectCell, MINE, KING } from './gameSlice';
+
+export function BoardPawn({ state }) {
+  const className = 'BoardPawn' + (state === null ? ' Empty' : ((state.piece === KING ? ' King' : ' Pawn') + (state.team === MINE ? ' Mine' : ' Theirs')));
+  return (
+    <svg className={className} height='25' width='25'>
+      {
+        state === null
+        ? <></>
+        : (
+          state.piece === KING
+          ? <polygon points='2.5,22.5 22.5,22.5 12.5,2.5' />
+          : <circle cx='12.5' cy='12.5' r='10' />
+        )
+      }
+    </svg>
+  )
+}
 
 export function BoardCell({ state, row, col }) {
   const dispatch = useDispatch();
@@ -8,9 +25,10 @@ export function BoardCell({ state, row, col }) {
     dispatch(selectCell({row, col}));
   }
 
+  const className = 'BoardCell' + (row === 0 && col === 2 ? ' Theirs' : (row === 4 && col === 2 ? ' Mine' : ''));
   return (
-    <td className="BoardCell" onClick={onClick}>
-      { state === null ? 0 : (2 * state.team + (state.piece === KING) + 1) }
+    <td className={className} onClick={onClick}>
+      <BoardPawn state={state} />
     </td>
   )
 }
