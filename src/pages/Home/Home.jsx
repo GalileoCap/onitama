dcc0fd8a98bc189789ec45240b50b2c317d475d5
GalileoCap <galileocapp@gmail.com>
2023-03-14 22:00:22 -0300

    OK: GamesList as cards

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,37 @@
-import { Link } from 'react-router-dom';
+import { Card, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import './Home.css';
+
+function GameCard({ name, description, url }) {
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate(url);
+  }
+  const onPlay = () => {
+    navigate(url + '/play'); //TODO: #play
+  }
+  const onRules = () => {
+    navigate(url + '#rules');
+  }
+
+  return (
+    <Card className='GameCard' onClick={onClick}>
+      <Card.Title>{name}</Card.Title>
+      <Card.Img variant='top' src='./holder.svg' />
+      <Card.Text>{description}</Card.Text>
+      <div className='card-actions'>
+        <Button onClick={onPlay} variant='primary'>Play Now!</Button>
+        <Button onClick={onRules} variant='secondary'>Rules</Button>
+      </div>
+    </Card>
+  );
+}
 
 function GamesList() {
   return (
-    <nav className='GamesList'>
-      <ul>
-        <li><Link to='onitama'>Onitama</Link></li>
-        <li><Link to='tiictaactooee'>TiicTaacTooee</Link></li>
-      </ul>
+    <nav id='GamesList'>
+      <GameCard name='Onitama' url='onitama' description='Like chess!' />
+      <GameCard name='TiicTaacTooee' url='tiictaactooee' description='TicTacToe^2' />
     </nav>
   );
 }
